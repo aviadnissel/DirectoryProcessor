4498f4f9b1f71307628328e545675e2b924a081c
Noy <noystll@gmail.com>
2017-05-23 08:37:37 +0300

    Started to work on Main

--- src/fileprocessing/DirectoryProcessor.java
+++ src/fileprocessing/DirectoryProcessor.java
@@ -1,9 +1,10 @@
 /**
- * Created by Noy Sternlicht on 20/05/2017.
+ * @Author: Noy Sternlicht
  */
 
 package fileprocessing;
 import fileprocessing.exceptions.errors.*;
+import fileprocessing.exceptions.warnings.FileProcessingWarning;
 import fileprocessing.sections.Section;
 import java.io.File;
 import java.util.List;
@@ -25,11 +26,22 @@ public class DirectoryProcessor {
 
     /* --- Main Methods --- */
 
-    private static List<String> splitToSections(File commandFile){
+    /**
+     * Runs invalidUsage test and ioProblems test and handle the errors if there are.
+     * @param args
+     * @returns: If the commands file passed the tested, returns is. Throws the relevant error otherwise.
+     */
+    private static File runBasicTests(String[] args) throws FileProcessingError{
         return null;
     }
 
-    private static List<Section> createSections(List<String> sectionsText){
+    private static List<String> splitToSections(File commandFile){
+
+        return null;
+    }
+
+    private static List<Section> createSections(List<String> sectionsText) throws FileProcessingError,
+            FileProcessingWarning{  //Here the program catches the errors from section factory & the warnings from F.factory, O.factory.
         return null;
     }
 
@@ -39,7 +51,14 @@ public class DirectoryProcessor {
 
     /* --- MAIN --- */
 
-    public static void main(String[] args) throws FileProcessingError{
+    public static void main(String[] args) throws FileProcessingError, FileProcessingWarning{
+
+        File commandsFile = runBasicTests(args); // If the file passed the tests, it is saved as a variable.
+        List<String> sectionsTextList = splitToSections(commandsFile);
+        List<Section> sectionsList = createSections(sectionsTextList);
+
+        printOutput(sectionsList);
+
     }
 }
 
